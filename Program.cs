4f6cea4ab9f8a3a467a32475c2a0e6020a44d2be
Karthik C <ckarthikmuthu1234@gmail.com>
2022-07-23 12:29:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Blobs;
+using System;
+
+namespace DownloadAllBlops
+{
+    public class Program
+    {
+        static string connectionString = "Azure Storage account Connection string";
+        static string containerName = "repository-group1";
+         
+        public static void Main(string[] args)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            var blobs = containerClient.GetBlobs();
+
+            foreach (var blob in blobs)
+            {
+                Console.WriteLine(blob.Name);
+                BlobClient blobClient = containerClient.GetBlobClient(blob.Name);
+                blobClient.DownloadTo(@"C:\downloadedBlob\" + blob.Name);
+
+            }
+            Console.Read();
+        }
+    }
+}
